5365d936988a0cf286d42ad579837609cb9cf3da
r0mmmy <em0reas1@gmail.com>
2025-02-22 05:24:35 +0000

    practice

--- chatbot.py
+++ chatbot.py
@@ -2,37 +2,59 @@
 from dotenv import load_dotenv
 load_dotenv()
 
-
+# 모델 초기화
 from langchain.chat_models import init_chat_model
 model = init_chat_model("gpt-4o-mini", model_provider="openai")
 
 from langchain_core.messages import HumanMessage
-
 from langgraph.checkpoint.memory import MemorySaver
 from langgraph.graph import START, MessagesState, StateGraph
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 
-# Define a new graph
-workflow = StateGraph(state_schema=MessagesState)
+prompt_template = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            "You are a helpful assistant. Answer all questions to the best of your ability in {language}.",
+        ),
+        MessagesPlaceholder(variable_name="messages"),
+    ]
+)
+
+from typing import Sequence
+
+from langchain_core.messages import BaseMessage
+from langgraph.graph.message import add_messages
+from typing_extensions import Annotated, TypedDict
 
 
-# Define the function that calls the model
-def call_model(state: MessagesState):
-    response = model.invoke(state["messages"])
-    return {"messages": response}
+class State(TypedDict):
+    messages: Annotated[Sequence[BaseMessage], add_messages]
+    language: str
 
 
-# Define the (single) node in the graph
+workflow = StateGraph(state_schema=State)
+
+
+def call_model(state: State):
+    prompt = prompt_template.invoke(state)
+    response = model.invoke(prompt)
+    return {"messages": [response]}
+
+
 workflow.add_edge(START, "model")
 workflow.add_node("model", call_model)
 
-# Add memory
 memory = MemorySaver()
 app = workflow.compile(checkpointer=memory)
 
-config = {"configurable": {"thread_id": "abc123"}}
-
+config = {"configurable": {"thread_id": "abc456"}}
 query = "Hi! I'm Bob."
+language = "Spanish"
 
 input_messages = [HumanMessage(query)]
-output = app.invoke({"messages": input_messages}, config)
-output["messages"][-1].pretty_print()  # output contains all messages in state
+output = app.invoke(
+    {"messages": input_messages, "language": language},
+    config,
+)
+output["messages"][-1].pretty_print()
